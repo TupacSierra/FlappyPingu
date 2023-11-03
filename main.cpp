59d3a798505bf58a1cef74e12f7c464499f02e8f
unknown <tupacas1998@gmail.com>
2023-11-03 12:56:51 -0300

    Init Code

--- main.cpp
+++ main.cpp
@@ -1,51 +1,73 @@
 #include "raylib.h"
 
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - keyboard input");
+    InitWindow(screenWidth, screenHeight, "FlappyPingu");
 
-    Vector2 ballPosition = { (float)screenWidth / 2, (float)screenHeight / 2 };
+    Vector2 ballPosition = { (float)screenWidth - 700 , (float)screenHeight / 2 };
+    const float ballRadius = 50.0f;
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
+    Rectangle tube;
+    tube.x = static_cast<float> (GetScreenWidth());
+    tube.y = static_cast<float>(GetScreenHeight()) / 2 - 25;
+    tube.width = 75;
+    tube.height = 200;
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    Vector2 tubePosition = { tube.x,tube.y };
+
+    const float speed = 250.0f;
+
+    bool collided = false;
+
+
+    while (!WindowShouldClose())    
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
-        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
-        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
-        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
-        //----------------------------------------------------------------------------------
+      
+      
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= 120.0f * GetFrameTime();
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 120.0f * GetFrameTime();
+       
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        tube.x -= speed * GetFrameTime();
+
+        if (tube.x + tube.width < 0)
+        {
+
+            tube.x = static_cast<float>(GetScreenWidth());
+            tube.y = static_cast<float>(GetRandomValue(0, GetScreenHeight() - 50));
+
+        }
+
+        collided = CheckCollisionCircleRec(Vector2{ ballPosition.x, ballPosition.y }, ballRadius, tube);
+       
+       
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+        DrawText("version 0.1 flappypingu", 10, 430, 10, DARKGRAY);
 
         DrawCircleV(ballPosition, 50, MAROON);
 
+        DrawRectangleRec(tube, BLUE);
+
+        DrawText("DONT TOUCH THE BLUE RECTAGLE", screenWidth / 2, 10, 20, BLACK);
+
+        if (collided)
+        {
+            DrawText("You lose", 10, 10, 20, RED);
+        }
+
         EndDrawing();
-        //----------------------------------------------------------------------------------
+        
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    
+    CloseWindow();     
+    
 
     return 0;
 }
