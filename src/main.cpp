4c9e9166324f663a7819aa93293b4860fff88614
Tupac Sierra <tupacas1998@gmail.com>
2023-11-14 14:12:13 -0300

    Add menu,credits and playgame code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include "raylib.h"
 
+enum GameState {
+    MENU,
+    PLAY,
+    CREDITS,
+    EXIT
+};
+
 int main(void)
 {
     const int screenWidth = 750;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "FlappyPingu");
-
-
 
     Texture2D background = LoadTexture("res/layers/sky.png");
     Texture2D background2 = LoadTexture("res/layers/cloud_lonely.png");
@@ -16,7 +21,6 @@ int main(void)
     Texture2D midground2 = LoadTexture("res/layers/clouds_mg_2.png");
     Texture2D foreground = LoadTexture("res/layers/clouds_mg_1_lightened.png");
 
-
     float scrollingBack = 0.0f;
     float scrollingBack2 = 0.0f;
     float scrollingBack3 = 0.0f;
@@ -24,88 +28,103 @@ int main(void)
     float scrollingMid2 = 0.0f;
     float scrollingFore = 0.0f;
 
-
     Vector2 ballPosition = { (float)screenWidth - 600 , (float)screenHeight / 2 };
     const float ballRadius = 40.0f;
 
-    Rectangle upperTube1{ (float)screenWidth,0.0f ,75.0f ,0.0f};
-    Rectangle lowerTube1{ (float)screenWidth ,0.0f ,75.0f ,0.0f };
+    Rectangle upperTube1 = { (float)screenWidth, 0.0f, 95.0f, 0.0f };
+    Rectangle lowerTube1 = { (float)screenWidth, 0.0f, 95.0f, 0.0f };
 
-    Rectangle upperTube2{ (float)screenWidth + screenWidth / 2 ,0.0f ,75.0f ,0.0f }; 
-    Rectangle lowerTube2{ (float)screenWidth + screenWidth / 2 ,0.0f ,75.0f ,0.0f };
+    Rectangle upperTube2 = { (float)screenWidth + screenWidth / 2, 0.0f, 95.0f, 0.0f };
+    Rectangle lowerTube2 = { (float)screenWidth + screenWidth / 2, 0.0f, 95.0f, 0.0f };
 
     upperTube1.width = lowerTube1.width = upperTube2.width = lowerTube2.width = 95;
-   
 
-
-    const float tubeSpeed = 400.0f;  
-
+    const float tubeSpeed = 400.0f;
     const float gravity = 650.0f;
     const float jumpSpeed = 200.0f;
 
-   
     float ballVelocityY = 0.0f;
 
     bool collided = false;
 
-    while (!WindowShouldClose())    
+    GameState gameState = MENU;
+
+    while (!WindowShouldClose())
     {
-     
-
-        const float deltaTime = GetFrameTime();
-
-        
-        if (IsKeyPressed(KEY_SPACE) && ballPosition.y > ballRadius)
+        switch (gameState)
         {
-            ballVelocityY = -jumpSpeed;
+        case MENU:
+            if (IsKeyPressed(KEY_P)) {
+                gameState = PLAY;
+            }
+            else if (IsKeyPressed(KEY_C)) {
+                gameState = CREDITS;
+            }
+            else if (IsKeyPressed(KEY_ESCAPE)) {
+                gameState = EXIT;
+            }
+            break;
+
+        case PLAY:
+            if (IsKeyPressed(KEY_SPACE) && ballPosition.y > ballRadius) {
+                ballVelocityY = -jumpSpeed;
+            }
+
+            ballPosition.y += ballVelocityY * GetFrameTime();
+            ballVelocityY += gravity * GetFrameTime();
+
+            upperTube1.x = lowerTube1.x -= tubeSpeed * GetFrameTime();
+            upperTube2.x = lowerTube2.x -= tubeSpeed * GetFrameTime();
+
+            if (upperTube2.x + upperTube2.width <= 0) {
+             
+                upperTube2.x = lowerTube2.x = static_cast<float>(GetScreenWidth());
+                float tubePosition = static_cast<float>(GetRandomValue(150, GetScreenHeight() - 200));
+                float gapHeight = static_cast<float>(GetRandomValue(60, 70));
+                float tubeHeight = static_cast<float>(GetScreenHeight()) - tubePosition + gapHeight;
+                upperTube2.height = tubePosition - gapHeight;
+                lowerTube2.y = tubePosition + gapHeight;
+                lowerTube2.height = tubeHeight;
+            }
+
+            if (upperTube1.x + upperTube1.width <= 0) {
+              
+                upperTube1.x = lowerTube1.x = static_cast<float>(GetScreenWidth());
+                float tubePosition = static_cast<float>(GetRandomValue(150, GetScreenHeight() - 200));
+                float gapHeight = static_cast<float>(GetRandomValue(60, 70));
+                float tubeHeight = static_cast<float>(GetScreenHeight()) - tubePosition + gapHeight;
+                upperTube1.height = tubePosition - gapHeight;
+                lowerTube1.y = tubePosition + gapHeight;
+                lowerTube1.height = tubeHeight;
+            }
+
+            collided = CheckCollisionCircleRec(Vector2{ ballPosition.x, ballPosition.y }, ballRadius, upperTube1) ||
+                CheckCollisionCircleRec(Vector2{ ballPosition.x, ballPosition.y }, ballRadius, lowerTube1) ||
+                CheckCollisionCircleRec(Vector2{ ballPosition.x, ballPosition.y }, ballRadius, upperTube2) ||
+                CheckCollisionCircleRec(Vector2{ ballPosition.x, ballPosition.y }, ballRadius, lowerTube2);
+            break;
+
+        case CREDITS:
+            DrawText("Game Code by Tupac Sierra", screenWidth / 2 - 70, screenHeight / 2 - 20, 20, WHITE);
+            DrawText("Art background by @vnitti_art ", screenWidth / 2 - 90, screenHeight / 2 + 20, 20, WHITE);
+            DrawText("Press B to go back to the menu", screenWidth / 2 - 60, screenHeight / 2 + 60, 20, WHITE);
+
+            if (IsKeyPressed(KEY_B)) {
+                gameState = MENU;
+            }
+            break;
+
+        case EXIT:
+            CloseWindow();
+            break;
         }
 
-        ballPosition.y += ballVelocityY * deltaTime;
-        ballVelocityY += gravity * deltaTime;
-        
-        upperTube1.x = lowerTube1.x -= tubeSpeed * deltaTime;
-        upperTube2.x = lowerTube2.x -= tubeSpeed * deltaTime;
-
-
-
-        if (upperTube2.x + upperTube2.width <= 0)
-        {
-
-            upperTube2.x = lowerTube2.x = static_cast <float>(GetScreenWidth());
-            float tubePosition = static_cast<float>(GetRandomValue(150, GetScreenHeight() - 200));
-            float gapHeight = static_cast<float>(GetRandomValue(60, 70));
-            float tubeHeight = static_cast<float>(GetScreenHeight()) - tubePosition + gapHeight;
-            upperTube2.height = tubePosition - gapHeight;
-            lowerTube2.y = tubePosition + gapHeight;
-            lowerTube2.height = tubeHeight;
-    
-        }
-
-        if (upperTube1.x + upperTube1.width <= 0)
-        {
-
-            upperTube1.x = lowerTube1.x = static_cast <float>(GetScreenWidth());
-            float tubePosition = static_cast<float>(GetRandomValue(150, GetScreenHeight() - 200));
-            float gapHeight = static_cast<float>(GetRandomValue(60, 70));
-            float tubeHeight = static_cast<float>(GetScreenHeight()) - tubePosition + gapHeight;
-            upperTube1.height = tubePosition - gapHeight;
-            lowerTube1.y = tubePosition + gapHeight;
-            lowerTube1.height = tubeHeight;
-
-        }
-
-        collided = CheckCollisionCircleRec(Vector2{ ballPosition.x, ballPosition.y }, ballRadius, upperTube1) ||
-                   CheckCollisionCircleRec(Vector2{ ballPosition.x, ballPosition.y }, ballRadius, lowerTube1) ||
-                   CheckCollisionCircleRec(Vector2{ ballPosition.x, ballPosition.y }, ballRadius, upperTube2) ||
-                   CheckCollisionCircleRec(Vector2{ ballPosition.x, ballPosition.y }, ballRadius, lowerTube2) ;
-
-
-        scrollingBack -= 0.1f * deltaTime;
-        scrollingBack2 -= 20.0f * deltaTime;
-        scrollingBack3 -= 35.0f * deltaTime;
-        scrollingMid -= 70.0f * deltaTime;
-        scrollingMid2 -= 85.0f * deltaTime;
-        scrollingFore -= 130.0f * deltaTime;
+        scrollingBack -= 0.1f * GetFrameTime();
+        scrollingBack2 -= 20.0f * GetFrameTime();
+        scrollingBack3 -= 35.0f * GetFrameTime();
+        scrollingMid -= 70.0f * GetFrameTime();
+        scrollingMid2 -= 85.0f * GetFrameTime();
+        scrollingFore -= 130.0f * GetFrameTime();
 
         if (scrollingBack <= -background.width * 2) scrollingBack = 0;
         if (scrollingBack2 <= -background2.width * 2) scrollingBack2 = 0;
@@ -113,8 +132,6 @@ int main(void)
         if (scrollingMid <= -midground.width * 2) scrollingMid = 0;
         if (scrollingMid2 <= -midground2.width * 2) scrollingMid2 = 0;
         if (scrollingFore <= -foreground.width * 2) scrollingFore = 0;
-
-
 
         BeginDrawing();
 
@@ -125,16 +142,15 @@ int main(void)
 
         DrawTextureEx(background2, { scrollingBack2 , 20 }, 0.0f, 2.0f, WHITE);
         DrawTextureEx(background2, { background2.width * 2 + scrollingBack2, 20 }, 0.0f, 2.0f, WHITE);
-       
+
         DrawTextureEx(background3, { scrollingBack3 , 20 }, 0.0f, 2.0f, WHITE);
         DrawTextureEx(background3, { background3.width * 2 + scrollingBack3, 20 }, 0.0f, 2.0f, WHITE);
 
-        DrawTextureEx(midground,  { scrollingMid, 40 }, 0.0f, 2.0f, WHITE);
+        DrawTextureEx(midground, { scrollingMid, 40 }, 0.0f, 2.0f, WHITE);
         DrawTextureEx(midground, { midground.width * 2 + scrollingMid, 40 }, 0.0f, 2.0f, WHITE);
 
         DrawTextureEx(midground2, { scrollingMid2, 40 }, 0.0f, 2.0f, WHITE);
         DrawTextureEx(midground2, { midground2.width * 2 + scrollingMid2, 40 }, 0.0f, 2.0f, WHITE);
-
 
         DrawCircleV(ballPosition, ballRadius, MAROON);
 
@@ -145,13 +161,35 @@ int main(void)
         DrawRectangleRec(lowerTube2, BLUE);
 
         DrawTextureEx(foreground, { scrollingFore, 50 }, 0.0f, 2.0f, WHITE);
-        DrawTextureEx(foreground, { foreground.width * 2 + scrollingFore, 50 }, 0.0f, 2.0f, WHITE); 
+        DrawTextureEx(foreground, { foreground.width * 2 + scrollingFore, 50 }, 0.0f, 2.0f, WHITE);
 
 
-        DrawText("Version 0.1 Flappy_Pingu", 10, 435, 15,BLACK);
-     
-   
-        DrawText("DONT TOUCH THE BLUE RECTAGLE", screenWidth / 2, 10, 10, WHITE);
+        switch (gameState)
+        {
+        case MENU:
+            DrawText("Press P to Play", screenWidth / 2 - 70, screenHeight / 2 - 20, 20, WHITE);
+            DrawText("Press C for Credits", screenWidth / 2 - 90, screenHeight / 2 + 20, 20, WHITE);
+            DrawText("Press ESC to Exit", screenWidth / 2 - 80, screenHeight / 2 + 60, 20, WHITE);
+
+            DrawText("Version 0.2 Flappy_Pingu", 10, 435, 15, BLACK);
+
+            DrawText("Flappy Pingu", screenWidth / 2 - 120, screenHeight / 2 - 80, 40, WHITE);
+            break;
+
+        case PLAY:
+           
+            break;
+
+        case CREDITS:
+            DrawText("Game Code by Tupac Sierra", screenWidth / 2 - 70, screenHeight / 2 - 20, 20, WHITE);
+            DrawText("Art background by @vnitti_art ", screenWidth / 2 - 90, screenHeight / 2 + 20, 20, WHITE);
+            DrawText("Press B to go back menu", screenWidth / 2 - 60, screenHeight / 2 + 60, 20, WHITE);
+            break;
+
+        case EXIT:
+            
+            break;
+        }
 
         if (collided)
         {
@@ -159,16 +197,14 @@ int main(void)
         }
 
         EndDrawing();
-        
     }
 
-    UnloadTexture(background);  
-    UnloadTexture(midground);  
+   
+    UnloadTexture(background);
+    UnloadTexture(midground);
     UnloadTexture(foreground);
 
-
-    CloseWindow();     
-    
+    CloseWindow();
 
     return 0;
 }
